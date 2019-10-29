69bbb5d10aaeae63988e0b3aabf9bd20635daa7f
Geoffrey Kirk <Geoffrey_Kirk@Trimble.com>
2019-10-28 19:25:19 -0600

    Add primary support for reading CSG Test Suite Logfiles

--- RTCM3_Decode.py
+++ RTCM3_Decode.py
@@ -2,10 +2,13 @@
 
 import sys
 import argparse
-import pprint
+from pprint import pprint
+import re
 
 import RTCM3
 from RTCM3_Decls import *
+import binascii
+import string
 
 
 # ByteToHex From http://code.activestate.com/recipes/510399-byte-to-hex-and-hex-to-byte-string-conversion/
@@ -98,31 +101,119 @@ if args.Verbose:
 
 #input_file=open ('RTCM3.bin','rb')
 #new_data = bytearray(input_file.read(255))
-new_data = bytearray(sys.stdin.read(1))
-while (new_data):
 
-    rtcm3.add_data (data=new_data)
-#    new_data=""
-    if len(rtcm3.buffer):
-#        print str(len(rtcm3.buffer))
-        sys.stdout.flush()
-    result = rtcm3.process_data (dump_decoded=False)
-    while result != 0 :
-#        print str(datetime.now())
-        if result == Got_Undecoded :
-            if Dump_Undecoded :
-                print "Undecoded Data: " +ByteToHex(rtcm3.undecoded);
-        elif result == Got_Packet :
-            rtcm3.dump(dump_undecoded=Dump_Undecoded,dump_decoded=Dump_Decoded,dump_timestamp=Dump_TimeStamp);
-            sys.stdout.flush()
-        else :
-                print "INTERNAL ERROR: Unknown result (" + str (result) + ")";
-                sys.exit();
-#        print "processing"
-        result = rtcm3.process_data ()
-#        print "processed: " + str(result)
-    new_data = sys.stdin.read(1)
-#    new_data = input_file.read(255)
+CSG_Test=False
+Looking_For_Frame=0
+Looking_For_Data_Start=1
+In_Data=2
+
+if CSG_Test:
+   RTCM_Frame_Line=re.compile(".*Receive RTCM v3 Frame Length (.+), Type (.+),")
+   RTCM_Data_Single_Line=re.compile("RTCM v3 Data = \[(.*)\]")
+   RTCM_Data_Line=re.compile("RTCM v3 Data = \[(.*)")
+   RTCM_End_Data_Line=re.compile(" *(.*)\]")
+   RTCM_State=Looking_For_Frame
+   RTCM_Data=None
+
+
+   for line in sys.stdin:
+      line=line.rstrip()
+#     print "line:",line
+      """
+      if RTCM_State == 0:
+         print "Looking for Frame"
+      elif RTCM_State == 1:
+         print "Looking for Data Start"
+      elif RTCM_State == 2:
+         print "Looking for Data End"
+      else:
+         print "Error state", RTCM_State
+      print "DATA", RTCM_Data
+"""
+      Regex = RTCM_Frame_Line.match(line) # Look for the start frame always
+
+      if Regex:
+#         print "Frame   Regex"
+         RTCM_Frame_Type=int(Regex.group(2))
+         rtcm3.packet_ID=RTCM_Frame_Type
+         print "Frame   ", line
+         RTCM_State=Looking_For_Data_Start
+         RTCM_Data=None
+
+      else:
+         if RTCM_State==Looking_For_Data_Start:
+            Regex = RTCM_Data_Single_Line.match(line)
+            if Regex:
+                  RTCM_Data=Regex.group(1)
+                  RTCM_Data=string.replace(RTCM_Data," ","")
+                  RTCM_Data=string.replace(RTCM_Data,"-","")
+#                  print "Single RTCM Data, ", RTCM_Frame_Type, RTCM_Data
+                  RTCM_Data=binascii.unhexlify(RTCM_Data)
+                  RTCM_Data=bytearray(RTCM_Data)
+   #               print "Frame   Regex"
+   #               print "Frame   ", line
+                  rtcm3.decode(RTCM_Frame_Type,RTCM_Data)
+                  rtcm3.dump(dump_undecoded=Dump_Undecoded,dump_decoded=Dump_Decoded,dump_timestamp=Dump_TimeStamp);
+                  RTCM_State=Looking_For_Frame
+                  RTCM_Data=None
+
+            else:
+               Regex=RTCM_Data_Line.match(line)
+               if Regex:
+#                     print "Start of Data    ", line
+                     RTCM_State=In_Data
+                     RTCM_Data=Regex.group(1)
+#                     print RTCM_Data
+         else:
+            if RTCM_State==In_Data:
+               Regex=RTCM_End_Data_Line.match(line)
+               if Regex:
+#                     print "Found End ", line
+                     RTCM_State=Looking_For_Frame
+                     RTCM_Data+=" " + Regex.group(1)
+                     RTCM_Data=string.replace(RTCM_Data," ","")
+                     RTCM_Data=string.replace(RTCM_Data,"-","")
+#                     print "End RTCM Data, ", RTCM_Data
+                     RTCM_Data=binascii.unhexlify(RTCM_Data)
+                     RTCM_Data=bytearray(RTCM_Data)
+#                     print RTCM_Data
+
+                     rtcm3.decode(RTCM_Frame_Type,RTCM_Data)
+                     rtcm3.dump(dump_undecoded=Dump_Undecoded,dump_decoded=Dump_Decoded,dump_timestamp=Dump_TimeStamp);
+                     RTCM_State=Looking_For_Frame
+                     RTCM_Data=None
+
+               else:
+#                  print "In Data ", line
+                  RTCM_Data+=" " + line.lstrip()
+#                  print RTCM_Data
+
+else:
+   new_data = bytearray(sys.stdin.read(1))
+   while (new_data):
+
+       rtcm3.add_data (data=new_data)
+   #    new_data=""
+       if len(rtcm3.buffer):
+   #        print str(len(rtcm3.buffer))
+           sys.stdout.flush()
+       result = rtcm3.process_data (dump_decoded=False)
+       while result != 0 :
+   #        print str(datetime.now())
+           if result == Got_Undecoded :
+               if Dump_Undecoded :
+                   print "Undecoded Data: " +ByteToHex(rtcm3.undecoded) + " Length (%)".format(len(rtcm3.undecoded));
+           elif result == Got_Packet :
+               rtcm3.dump(dump_undecoded=Dump_Undecoded,dump_decoded=Dump_Decoded,dump_timestamp=Dump_TimeStamp);
+               sys.stdout.flush()
+           else :
+                   print "INTERNAL ERROR: Unknown result (" + str (result) + ")";
+                   sys.exit();
+   #        print "processing"
+           result = rtcm3.process_data ()
+   #        print "processed: " + str(result)
+       new_data = sys.stdin.read(1)
+   #    new_data = input_file.read(255)
 
 print "Bye"
 
